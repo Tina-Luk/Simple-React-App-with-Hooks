38c476f543ab61198a9508d80bd72448d3686f00
Tina-Luk <tina.luk.tina@gmail.com>
2021-06-04 05:04:56 -0700

    useEffect component did mount and unmount

--- react-hooks-tutorial/src/App.js
+++ react-hooks-tutorial/src/App.js
@@ -1,13 +1,30 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useForm } from './useForm';
+import { Hello } from './Hello';
 
 const App = () => {
-	const [values, handleChange] = useForm({ email: '', password: '' });
+	const [values, handleChange] = useForm({
+		email: '',
+		password: '',
+		firstName: '',
+	});
+	const [showHello, setShowHello] = useState(true);
+	// const [values2, handleChange2] = useForm({ firstName: '', lastName: '' });
+
+	// useEffect(() => {
+	// 	console.log('render');
+	// 	return () => {
+	// 		console.log('unmount');
+	// 	};
+	// }, []);
 
 	return (
 		<div>
-			<input name="email" value={values.email} onChange={handleChange} />
-			<input type="password" name="password" value={values.password} onChange={handleChange} />
+			<button onClick={() => setShowHello(!showHello)}>toggle</button>
+			{showHello && <Hello />}
+			<input name="email" value={values.email} placeholder="Email" onChange={handleChange} />
+			<input name="firstName" value={values.firstName} placeholder="First Name" onChange={handleChange} />
+			<input type="password" name="password" placeholder="Password" value={values.password} onChange={handleChange} />
 		</div>
 	);
 };
