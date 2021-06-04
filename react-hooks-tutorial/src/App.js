ea3833b80a94fcf80c124a9173884076413ad015
Tina-Luk <tina.luk.tina@gmail.com>
2021-06-04 04:27:49 -0700

    Use state basic

---
+++ react-hooks-tutorial/src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+const App = () => {
+	const [{ count, count2 }, setCount] = useState({ count: 10, count2: 20 });
+
+	return (
+		<div>
+			<button
+				onClick={() =>
+					setCount((currentState) => ({
+						...currentState,
+						count: currentState.count + 1,
+						count2: currentState.count2 + 1,
+					}))
+				}
+			>
+				+
+			</button>
+			<div>count 1: {count}</div>
+			<div>count 2: {count2}</div>
+		</div>
+	);
+};
+
+export default App;
