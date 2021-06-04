1b3eebd1a6b4393a430728eeade5f281a973e4ec
Tina-Luk <tina.luk.tina@gmail.com>
2021-06-04 05:20:03 -0700

    useFetch with API and custom hooks

--- react-hooks-tutorial/src/App.js
+++ react-hooks-tutorial/src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useForm } from './useForm';
 import { Hello } from './Hello';
+import { useFetch } from './useFetch';
 
 const App = () => {
 	const [values, handleChange] = useForm({
@@ -8,20 +9,19 @@ const App = () => {
 		password: '',
 		firstName: '',
 	});
-	const [showHello, setShowHello] = useState(true);
-	// const [values2, handleChange2] = useForm({ firstName: '', lastName: '' });
 
-	// useEffect(() => {
-	// 	console.log('render');
-	// 	return () => {
-	// 		console.log('unmount');
-	// 	};
-	// }, []);
+	const [count, setCount] = useState(0);
+
+	const { data, loading } = useFetch(`http://numbersapi.com/${count}/trivia`);
 
 	return (
 		<div>
-			<button onClick={() => setShowHello(!showHello)}>toggle</button>
-			{showHello && <Hello />}
+			<div>{loading ? 'loading...' : data}</div>
+			<div>count: {count}</div>
+			<button onClick={() => setCount((c) => c + 1)}>increment</button>
+
+			{/* <button onClick={() => setShowHello(!showHello)}>toggle</button> */}
+			{/* {showHello && <Hello />} */}
 			<input name="email" value={values.email} placeholder="Email" onChange={handleChange} />
 			<input name="firstName" value={values.firstName} placeholder="First Name" onChange={handleChange} />
 			<input type="password" name="password" placeholder="Password" value={values.password} onChange={handleChange} />
